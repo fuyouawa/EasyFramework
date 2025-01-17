536e5f7fc479142b3024aae0158ad87c2374753f
计都 <fuyouawa@gmail.com>
2025-01-17 12:07:27 +0800

    优化项目结构

--- Assets/Plugins/EasyFramework/Scripts/Feedbacks/Core/EasyFeedbacks.cs
+++ Assets/Plugins/EasyFramework/Scripts/Feedbacks/Core/EasyFeedbacks.cs
@@ -1,13 +1,10 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Sirenix.OdinInspector;
 using UnityEngine;
-using Sirenix.Serialization;
 
 namespace EasyGameFramework
 {
-    public class EasyFeedbacks : SerializedMonoBehaviour
+    public class EasyFeedbacks : MonoBehaviour
     {
         public enum InitializationModes
         {
